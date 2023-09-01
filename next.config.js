1b45e0691d06d26387fd368c7556739a5ad0654b
FrazaAdityaWiguna <adityafraja12@gmail.com>
2023-09-01 18:29:53 +0700

    chore: add domain url and change package-lock

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["files.lili-style.io"],
+  },
 };
 
 module.exports = nextConfig;
